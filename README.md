a248f47b4d61db1d22078f75fb76f0c47de65afb
zopty <38828702+zopty@users.noreply.github.com>
2026-05-24 20:38:04 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# OSC4AU2
+
+[OpenSoundControl (OSC)](https://opensoundcontrol.stanford.edu/) を使って AviUtl2 のパラメーターを変化させる時間制御スクリプト
+
+# 仕様
+
+`UDP:9000` で OSC メッセージを受信します
+
+アドレスは `/osc4au2/<ID>` です（値はFloatのみ）
+
+`<ID>` は、時間制御の `ID` パラメータと連動しています
+
+時間制御の範囲は「開始点の値」と「最初の中間点の値」の間となります
+
+例） `/osc4au2/0/` へ `0.5` を送信すると、 `ID` が `0` に設定されている時間制御は「開始点の値」と「最初の中間点の値」のちょうど中間の値をとります
+
+# インストール
+
+以下の方法が提供されています：
+
+- AviUtl2カタログからインストール
+- リリース から `.au2pkg.zip` 形式のファイルを入手
+- 手動でビルド（後述）
+
+## [AviUtl2カタログ](https://github.com/Neosku/aviutl2-catalog) からインストール（おすすめ）
+
+上記アプリをインストールし、検索欄に `OSC4AU2` と入力してください
+
+出てきた `OSC4AU2` の `インストール` ボタンを押すと自動で導入されます
+
+## `.au2pkg.zip` ファイルからインストール
+
+[リリース](../../releases/latest) から `.au2pkg.zip` 形式のファイルをダウンロードし、 AviUtl2 のプレビュー画面にドラッグ＆ドロップしてください
+
+インストール完了後、自動で再起動されます
+
+## 手動でビルド
+
+> MSVC と Rust が必要です
+
+### 手順
+
+1. このリポジトリをクローン
+2. `cargo build`
+3. ファイルをコピー＆リネーム
+   - `target/debug (または release)/osc4au2.dll` → `Script/OSC4AU2.mod2`
+   - `script/OSC4AU2.tra2` → `Script/OSC4AU2.tra2`
---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+use aviutl2::{AnyResult, lprintln, module::ScriptModuleFunctions};
+use rosc::{OscPacket, OscType, decoder};
+use std::{
+    collections::HashMap,
+    net::UdpSocket,
+    sync::{Arc, OnceLock, RwLock},
+    thread,
+};
+
+type SharedMap = Arc<RwLock<HashMap<String, Vec<OscType>>>>;
+
+static OSC_THREAD_INIT: OnceLock<()> = OnceLock::new();
+
+fn start_osc_receiver(shared: SharedMap) {
+    OSC_THREAD_INIT.get_or_init(|| {
+        let shared = shared.clone();
+
+        thread::spawn(move || {
+            let socket = UdpSocket::bind("0.0.0.0:9000").expect("bind failed");
+
+            lprintln!("OSC Listening on 9000");
+
+            let mut buf = [0u8; rosc::decoder::MTU];
+
+            loop {
+                let (size, _) = match socket.recv_from(&mut buf) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        let _ = aviutl2::logger::write_error_log(&format!("recv error: {e}"));
+                        continue;
+                    }
+                };
+
+                let packet = match decoder::decode_udp(&buf[..size]) {
+                    Ok((_rem, packet)) => packet,
+                    Err(e) => {
+                        let _ = aviutl2::logger::write_error_log(&format!("decode error: {e}"));
+                        continue;
+                    }
+                };
+
+                handle_packet(packet, &shared);
+            }
+        });
+    });
+}
+
+fn handle_packet(packet: OscPacket, shared: &SharedMap) {
+    match packet {
+        OscPacket::Message(msg) => {
+            let mut map = shared.write().unwrap();
+
+            map.insert(msg.addr, msg.args);
+        }
+
+        OscPacket::Bundle(bundle) => {
+            for p in bundle.content {
+                handle_packet(p, shared);
+            }
+        }
+    }
+}
+
+#[aviutl2::plugin(ScriptModule)]
+struct OscReceiverModule {
+    shared: SharedMap,
+}
+
+impl aviutl2::module::ScriptModule for OscReceiverModule {
+    fn new(_info: aviutl2::common::AviUtl2Info) -> AnyResult<Self> {
+        let shared = SharedMap::default();
+
+        start_osc_receiver(shared.clone());
+
+        Ok(Self { shared })
+    }
+
+    fn plugin_info(&self) -> aviutl2::module::ScriptModuleTable {
+        aviutl2::module::ScriptModuleTable {
+            information: format!(
+                "OSC Receiver for AviUtl2 | v{version}",
+                version = env!("CARGO_PKG_VERSION")
+            ),
+            functions: Self::functions(),
+        }
+    }
+}
+
+#[aviutl2::module::functions]
+impl OscReceiverModule {
+    #[direct]
+    fn get(&self, params: &mut aviutl2::module::ScriptModuleCallHandle) {
+        let addr = params.get_param_str(0).unwrap_or_default();
+        if addr.is_empty() {
+            return;
+        }
+        if let Ok(map) = self.shared.read() {
+            if let Some(values) = map.get(&addr).cloned() {
+                for value in values {
+                    if let OscType::Float(v) = value {
+                        params.push_result_float(v.into());
+                    }
+                }
+            }
+        }
+    }
+}
+
+aviutl2::register_script_module!(OscReceiverModule);
